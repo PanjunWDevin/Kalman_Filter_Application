dc663ae2c5eb14f3554830e35ed48eb6589ddc0f
Panjun <wangpjdevin@gmail.com>
2016-08-06 13:44:53 +0800

    Update and rename KalmanFilter.py to KalmanFilter_Pair.py

--- KalmanFilter_Pair.py
+++ KalmanFilter_Pair.py
@@ -1,4 +1,4 @@
-#Pair Trading using Kalman Filter Framwork
+## Kalman Filter Application in Pair Trading
 __author__ = 'Panjun'
 #-*- coding: utf - 8 -*-
 # further explore pair trading strategy, consider using Kalman Filter to construct hedging strategy
@@ -48,43 +48,83 @@ pairs_selected.sort(key=itemgetter(2)) #ascending order thus the first two stock
 stock_1 = pairs_selected[0][0]  #China State Ship Building
 stock_2 = pairs_selected[0][1]  #China Railway Group
 
-print stock_1
-print stock_2
+#print stock_1
+#print stock_2
 
 # plot the price chart of stock_1 and stock_2
-stock_prices[stock_1].plot(label=stock_1,color='r')
-stock_prices[stock_2].plot(label=stock_2,color='b')
-plt.legend(loc='best')
-plt.xlabel('Date')
-plt.ylabel('Prices')
-plt.show()
+#stock_prices[stock_1].plot(label=stock_1,color='r')
+#stock_prices[stock_2].plot(label=stock_2,color='b')
+#plt.legend(loc='best')
+#plt.xlabel('Date')
+#plt.ylabel('Prices')
+#plt.show()
 
-# calculate zscores
-stable_series = stock_prices[stock_1] - stock_prices[stock_2]
-
-z_scores = (stable_series - stable_series.mean())/stable_series.std()
-
-plt.plot(z_scores,label='diff')
-#z_scores.plot(date_range,label ='diff')
-plt.axhline(z_scores.mean(), color='k')
-plt.axhline(1, color='r', linestyle='--')
-plt.axhline(-1, color='r', linestyle='--')
-plt.axhline(1.5, color='b', linestyle='--')
-plt.axhline(-1.5, color='b', linestyle='--')
-plt.axhline(2, color='g', linestyle='--')
-plt.axhline(-2, color='g', linestyle='--')
-plt.legend(loc='best')
-plt.xlabel('date')
-plt.ylabel('diff')
-plt.show()
 
 #static hedging, considering
 x = stock_prices[stock_1]
 y = stock_prices[stock_2]
 
-X = sts.add_constant(x)
-result = (sts.OLS(y,X)).fit()
-print (result.summary())
+#X = sts.add_constant(x)
+#result = (sts.OLS(y,X)).fit()
+#print (result.summary())
+# stock_2 = -0.8441 + 1.083 * stock_1
+coeff = 1.083
+
+#new_stock1_prices = np.linspace(x.min(),x.max(),100)
+#stock2_prices_hat = -0.8441 + 1.083*new_stock1_prices
+
+#plt.scatter(x,y)
+#plt.plot(new_stock1_prices,stock2_prices_hat,color ='r')
+#plt.show()
+
+##implement kalman filter
+from pykalman import KalmanFilter
+
+#be extremely careful that inputs best to be 'series'
+
+delta = 1e-3
+trans_cov = delta/(1-delta)*np.eye(2)
+obs_mat = np.expand_dims(np.vstack([[x], [np.ones(len(x))]]).T, axis=1)
+
+kf = KalmanFilter(n_dim_obs=1, n_dim_state=2, # stock2_p is one dimensional, (alpha beta) are two dimensional
+                 initial_state_mean=[0,0],
+                 initial_state_covariance=np.ones((2,2)),
+                 transition_matrices=np.eye(2),
+                 observation_matrices=obs_mat,
+                 observation_covariance=2,
+                 transition_covariance=trans_cov)
+
+#use the observations y stock2_p to get running estimatates and errors for the state parameters
+state_means, state_covs = kf.filter(y.values)
+
+pd.DataFrame(dict(slope=state_means[:,0], intercept=state_means[:,1]), index=date_range).plot(subplots=True)
+plt.tight_layout
+plt.show()
+print ('alpha :',np.mean(state_means[:,1]))
+print ('beta :',np.mean(state_means[:,0]))
+
+
+
+
+
+# calculate zscores
+#stable_series = stock_prices[stock_1] - stock_prices[stock_2]
+
+#z_scores = (stable_series - stable_series.mean())/stable_series.std()
+
+#plt.plot(z_scores,label='diff')
+#z_scores.plot(date_range,label ='diff')
+#plt.axhline(z_scores.mean(), color='k')
+#plt.axhline(1, color='r', linestyle='--')
+#plt.axhline(-1, color='r', linestyle='--')
+#plt.axhline(1.5, color='b', linestyle='--')
+#plt.axhline(-1.5, color='b', linestyle='--')
+#plt.axhline(2, color='g', linestyle='--')
+#plt.axhline(-2, color='g', linestyle='--')
+#plt.legend(loc='best')
+#plt.xlabel('date')
+#plt.ylabel('diff')
+#plt.show()
 
 
 
